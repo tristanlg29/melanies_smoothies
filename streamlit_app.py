91808f0e53bc0789f36bcff9df3a00e8c536b119
tristanlg29 <102234643+tristanlg29@users.noreply.github.com>
2025-02-24 16:08:05 +0100

    Update streamlit_app.py
    Ajout appel API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 helpful_links = [
@@ -46,6 +47,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
 
